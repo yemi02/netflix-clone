a5827fcc72d271ed8bde689d7590311b5b4cbef4
Yemi Adetola <yemi@Winter.local>
2024-07-25 11:17:28 +0100

    database connected

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import authRoutes from "./routes/auth.route.js";
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
+
+const PORT = ENV_VARS.PORT;
+
+const app = express();
+
+app.use("/api/v1/auth", authRoutes);
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost/${PORT}`);
+  connectDB();
+});
